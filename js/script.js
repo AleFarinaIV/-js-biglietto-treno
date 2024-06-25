2c545744f91fb018058605e3a1776c62570b2fbf
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-06-25 23:34:51 +0200

    [UPD] added IF structure and completed exercise

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-const kilometres = prompt ("Quanti chilometri vuoi percorrere?")
-const age = prompt ("Qunati anni hai?")
+const kilometres = Number (prompt ("Quanti chilometri vuoi percorrere?"))
+const age = Number (prompt ("Quanti anni hai?"))
 
 const fullPrice = kilometres * 0.21
+const fullPriceDecimal = fullPrice.toFixed(2)
+
+if (age < 18) {
+    discountPrice = fullPrice - (fullPrice * 20 / 100)
+    discountPriceDecimal = discountPrice.toFixed(2)
+    document.getElementById('discountPrice').innerHTML = "Il prezzo del suo biglietto sarà di " + discountPriceDecimal + "€"
+}else if (age > 65) {
+    discountPrice = fullPrice - (fullPrice * 40 / 100)
+    discountPriceDecimal = discountPrice.toFixed(2)
+    document.getElementById('discountPrice').innerHTML = "Il prezzo del suo biglietto sarà di " + discountPriceDecimal + "€"
+}else {
+    document.getElementById('fullPrice').innerHTML = "Il prezzo del suo biglietto sarà di " + fullPriceDecimal + "€"
+}
